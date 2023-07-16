579e92e4f37fbd960713d6a4ba2cfe212a9fed62
cekim <rlatldhszx@naver.com>
2023-07-17 08:45:09 +0900

    first Commit

---
+++ webDev_1/src/main/java/com/ell/WebDev1Application.java
@@ -0,0 +1,13 @@
+package com.ell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDev1Application.class, args);
+	}
+
+}
